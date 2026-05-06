a5847393627fc9c98ed9e3a287f4c9647f71d942
Jorim Tielemans <tielemans.jorim@gmail.com>
2026-05-06 12:56:14 +0200

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "2.3.21" apply false
     id("com.google.devtools.ksp") version "2.3.7" apply false
     id("com.google.dagger.hilt.android") version "2.59.2" apply false
-    id("dev.detekt") version "2.0.0-alpha.2" apply false
+    id("dev.detekt") version "2.0.0-alpha.3" apply false
     id("org.jlleitschuh.gradle.ktlint") version "14.2.0" apply false
 }
